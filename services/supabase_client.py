20966dd10f16addd7a401c9654ad272064a48444
xdificx <yskrawpts10@gmail.com>
2026-04-11 13:02:55 +0900

    Create supabase_client.py

---
+++ services/supabase_client.py
@@ -0,0 +1,6 @@
+from supabase import create_client
+
+SUPABASE_URL = "https://oilgudbecavjdfydnoot.supabase.co"
+SUPABASE_KEY = "sb_publishable_rVNgwabQ12fJcEUDuP8wsQ_hrD2GWPy"
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
