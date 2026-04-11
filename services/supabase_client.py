b614ac032ccb4daea263744797a2ec4a42e0829a
xdificx <yskrawpts10@gmail.com>
2026-04-11 14:35:57 +0900

    Update supabase_client.py

--- services/supabase_client.py
+++ services/supabase_client.py
@@ -1,6 +1,7 @@
+import os
 from supabase import create_client
 
-SUPABASE_URL = "https://iadwpshbyphyxnearpir.supabase.co"
-SUPABASE_KEY = "sb_publishable_9hYxK_tOLq4AuQ2FScS7vw_kSjdNzzT"
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
